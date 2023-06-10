68f9a61c367d674b24efb282f6f7554b6154558a
7H30D0R <marcusanholt@gmail.com>
2023-06-10 19:34:01 +0200

    Disable villages & map events

---
+++ source/GameInterface/Services/MapEvents/Patches/DisableMapEventPatches.cs
@@ -0,0 +1,47 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.MapEvents;
+using TaleWorlds.CampaignSystem.Party;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace GameInterface.Services.MapEvents.Patches
+{
+    [HarmonyPatch(typeof(MapEvent))]
+    internal class DisableMapEventPatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(MethodType.Constructor)]
+        private static bool DisableConstructor() => false;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(MapEvent.Initialize))]
+        private static bool DisableInitialize() => false;
+    }
+
+    [HarmonyPatch(typeof(StartBattleAction))]
+    internal class DisableStartBattleAction
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("ApplyInternal")]
+        private static bool DisableApplyInternal () => false;
+    }
+
+    [HarmonyPatch(typeof(EncounterManager))]
+    internal class DisableEncounterManagerMapEvents
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("StartSettlementEncounter")]
+        private static bool DisableStartSettlementEncounter(MobileParty attackerParty, Settlement settlement)
+        {
+            if (attackerParty.ShortTermBehavior == AiBehavior.AssaultSettlement && 
+                attackerParty.ShortTermTargetSettlement == settlement)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
---
+++ source/GameInterface/Services/Villages/Patches/DisableVillagePatches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using SandBox.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.CampaignBehaviors;
+using TaleWorlds.CampaignSystem.Settlements;
+
+namespace GameInterface.Services.Villages.Patches
+{
+    /// <summary>
+    /// Disables village functionality. Will be removed when
+    /// village synchronisation is ready to be implemented.
+    /// </summary>
+    [HarmonyPatch(typeof(Village))]
+    internal class DisableVillagePatches
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(Village.DailyTick))]
+        private static bool DisableDailyTick() => false;
+    }
+
+    [HarmonyPatch(typeof(VillagerCampaignBehavior))]
+    internal class DisableVillageCampaignBehavior
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(VillagerCampaignBehavior.RegisterEvents))]
+        private static bool DisableRegisterEvents() => false;
+    }
+
+    [HarmonyPatch(typeof(VillageGoodProductionCampaignBehavior))]
+    internal class DisabbleVillageGoodProductionCampaignBehavior
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(VillageGoodProductionCampaignBehavior.RegisterEvents))]
+        private static bool DisableRegisterEvents() => false;
+    }
+
+    [HarmonyPatch(typeof(VillageHealCampaignBehavior))]
+    internal class DisabbleVillageHealCampaignBehavior
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(VillageHealCampaignBehavior.RegisterEvents))]
+        private static bool DisableRegisterEvents() => false;
+    }
+}
